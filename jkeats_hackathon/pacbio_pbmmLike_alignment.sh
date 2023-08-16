7f569ceca25b037a35e883087ccc78fc7fa34b82
PedalheadPHX <jkeats@tgen.org>
2023-08-16 14:15:07 -0700

    First pbmm2 like suggestion

--- jkeats_hackathon/pacbio_pbmmLike_alignment.sh
+++ jkeats_hackathon/pacbio_pbmmLike_alignment.sh
@@ -22,17 +22,13 @@ PEPPER_MARGIN_SIF=/home/tgenref/containers/
 ###################################
 
 # Reference files and directory
-REFERENCE=/home/tgenref/homo_sapiens/grch38_hg38/hg38tgen/genome_reference/GRCh38tgen_decoy_alts_hla.fa
-ALT_FILE=/home/tgenref/homo_sapiens/grch38_hg38/hg38tgen/tool_resources/bwa2_2.2.1/GRCh38tgen_decoy_alts_hla.fa.alt
-REFERENCE_DIR=/home/tgenref/homo_sapiens/grch38_hg38/hg38tgen/genome_reference
+REFERENCE=/home/tgenref/homo_sapiens/t2t_chm13/chm13_v2/genome_reference/chm13v2.0_maskedY_rCRS.fa
+REFERENCE_DIR=/home/tgenref/homo_sapiens/t2t_chm13/chm13_v2/genome_reference/
+
 
 # Input file and directory
 INPUT_UBAM=/illumina_run_folders/pacbio/revio/01/r84132_20230804_002251/1_B01/hifi_reads/m84132_230804_011218_s2.hifi_reads.bc2009.bam
 INPUT_DIR=/illumina_run_folders/pacbio/revio/01/r84132_20230804_002251/1_B01/hifi_reads
-
-# Create readgroup information
-# NOTES: What is the CM tag? Will need a way to capture the DS tag string to include in updated uBAM. Is there a SMRT-cell identifier akin to flowcell ID?
-FULL_RG='@RG\tID:m84132_B01_bc2009\tPL:PACBIO\tPM:REVIO\tPU:m84132\tLB:L78024\tSM:COLO829BL_ATCCJJK_p1_CL_C2\tBC:CGATCGCACTGAGTAT\tCN:TGen\tDS:READTYPE=CCS;Ipd:Frames=ip;PulseWidth:Frames=pw;BINDINGKIT=102-739-100;SEQUENCINGKIT=102-118-800;BASECALLERVERSION=5.0;FRAMERATEHZ=100.000000;BarcodeFile=metadata/m84132_230804_011218_s2.barcodes.fasta;BarcodeHash=e7c4279103df8c8de7036efdbdca9008;BarcodeCount=113;BarcodeMode=Symmetric;BarcodeQuality=Score'
 
 # Output
 OUTPUT_BASENAME=/scratch/jkeats/revio/COLO829BL_ATCCJJK_p1_CL_C2
@@ -42,42 +38,8 @@ OUTPUT_DIR=/scratch/jkeats/revio
 TEMP=/scratch/jkeats/revio/tmp
 
 ###################################
-## Update the SMRT-link Created uBAM to have TGen Compliant RG tags and be ready for zipperBam
-###################################
-
-# Collate the BAM and update readgroups
-singularity exec -B $INPUT_DIR -B $OUTPUT_DIR -B $REFERENCE_DIR ${SAMTOOLS_SIF} \
-  samtools collate \
-  --threads 30 \
-  --no-PG \
-  -O \
-  -u \
-  ${INPUT_UBAM} | \
-  singularity exec -B $INPUT_DIR -B $OUTPUT_DIR ${SAMTOOLS_SIF} \
-    samtools addreplacerg \
-    --threads 10 \
-    --no-PG \
-    -r ${FULL_RG} \
-    -o ${OUTPUT_BASENAME}_uBAM_collated.bam \
-    --output-fmt BAM \
-    -
-
-###################################
 ## Align reads with minimap
 ###################################
-
-## THIS SECTION COULD BE CHUNKED TO OPTIMIZE COMPUTATION FOLLOWED BY MERGE
-
-# Create PacBio HiFi compatible index
-# IMPORTANT: precalculated index MUST match the presets used for alignment
-# ONT and HiFi indexes are NOT interchangeable
-singularity exec -B $OUTPUT_DIR -B $REFERENCE_DIR ${MINIMAP2_SIF} \
-  minimap2 \
-  -x map-hifi \
-  -d GRCh38tgen_decoy_alts_hla.ont.mmi \
-  --alt ${ALT_FILE} \
-  ${REFERENCE}
-
 
 # Create fastq from pacBio uBAM, pipe directly to minimap and convert to bam via samtools collate to prep for zipperBam
 # Need to review the PacBio settings particulary -Y
@@ -114,6 +76,8 @@ singularity exec -B $OUTPUT_DIR -B $REFERENCE_DIR ${MINIMAP2_SIF} \
 ## Additional variables altered by pbmm2
 # -u CHAR	How to find canonical splicing sites GT-AG - f: transcript strand; b: both strands; n: no attempt to match GT-AG [n]
 ### github says this is set but the example is incorrect as it should be -u [f/b/n]. Also why set for genomic alignments?
+### reading pbmm2 repo it seems like this is setting: Disable homopolymer-compressed k-mer (compression is active for SUBREAD and UNROLLED preset)
+### This should mean that -H IS NOT SET for HiFi/CCS reads unlike Subreads
 # -o FILE	Output alignments to FILE [stdout]
 ### they are setting the output file to be 6? No, they have a different internal parser instead of providing -O an myArray+=(item)
 # -z INT1[,INT2]  Truncate an alignment if the running alignment score drops too quickly along the diagonal of the DP matrix
@@ -130,6 +94,7 @@ singularity exec -B $OUTPUT_DIR -B $REFERENCE_DIR ${MINIMAP2_SIF} \
 ############################################
 
 # setting individual flags as expected by pbmm2 as preset might alter
+# allowing it to self create an index as the custom settings make me scared
 singularity exec -B $OUTPUT_DIR ${SAMTOOLS_SIF} \
   samtools fastq \
   --threads 10 \
@@ -138,20 +103,20 @@ singularity exec -B $OUTPUT_DIR ${SAMTOOLS_SIF} \
   singularity exec -B $OUTPUT_DIR -B $REFERENCE_DIR ${MINIMAP2_SIF} \
     minimap2 \
     -t 30 \
-    -k 19 -w 19 \
+    -k 19 -w 19 -O 6,26 -E 2,1 -A 1 -B 4 -z 400,50 -r 2000 -g 5000 \
     -a \
     -Y \
     -L \
     --eqx \
     --secondary=no \
-    GRCh38tgen_decoy_alts_hla.ont.mmi \
+    ${REFERENCE} \
     /dev/stdin \
     | \
     singularity exec -B $OUTPUT_DIR ${SAMTOOLS_SIF} \
       samtools collate \
       --threads 10 \
       --no-PG \
-      -o ${OUTPUT_BASENAME}_mm_collated.bam \
+      -o ${OUTPUT_BASENAME}_pbmmLike_collated.bam \
       --output-fmt BAM \
       -
 
@@ -159,7 +124,7 @@ singularity exec -B $OUTPUT_DIR ${SAMTOOLS_SIF} \
 # update RG tags using zipperBAM, sort the output, and create an index
 singularity exec -B $OUTPUT_DIR -B $REFERENCE_DIR ${FGBIO_SAMTOOLS_SIF} \
   fgbio --tmp-dir ${TEMP} --compression 1 --async-io=true ZipperBams \
-  --input=${OUTPUT_BASENAME}_mm_collated.bam \
+  --input=${OUTPUT_BASENAME}_pbmmLike_collated.bam \
   --unmapped=${OUTPUT_BASENAME}_uBAM_collated.bam \
   --ref=${REFERENCE} \
   | \
@@ -167,51 +132,7 @@ singularity exec -B $OUTPUT_DIR -B $REFERENCE_DIR ${FGBIO_SAMTOOLS_SIF} \
     samtools sort \
     --threads 20 \
     --reference ${REFERENCE} \
-    -o ${OUTPUT_BASENAME}_mm_rg_sort.cram \
-    --output-fmt CRAM \
-    --write-index \
-    -
-
-
-############################################
-# Minimap2 defaults with hifi preset
-############################################
-singularity exec -B $OUTPUT_DIR ${SAMTOOLS_SIF} \
-  samtools fastq \
-  --threads 10 \
-  ${OUTPUT_BASENAME}_uBAM_collated.bam \
-  | \
-  singularity exec -B $OUTPUT_DIR -B $REFERENCE_DIR ${MINIMAP2_SIF} \
-    minimap2 \
-    -t 30 \
-    -x map-hifi \
-    -a \
-    -Y \
-    -L \
-    GRCh38tgen_decoy_alts_hla.ont.mmi \
-    /dev/stdin \
-    | \
-    singularity exec -B $OUTPUT_DIR ${SAMTOOLS_SIF} \
-      samtools collate \
-      --threads 10 \
-      --no-PG \
-      -o ${OUTPUT_BASENAME}_mm_collated.bam \
-      --output-fmt BAM \
-      -
-
-
-# update RG tags using zipperBAM, sort the output, and create an index
-singularity exec -B $OUTPUT_DIR -B $REFERENCE_DIR ${FGBIO_SAMTOOLS_SIF} \
-  fgbio --tmp-dir ${TEMP} --compression 1 --async-io=true ZipperBams \
-  --input=${OUTPUT_BASENAME}_mm_collated.bam \
-  --unmapped=${OUTPUT_BASENAME}_uBAM_collated.bam \
-  --ref=${REFERENCE} \
-  | \
-  singularity exec -B $OUTPUT_DIR -B $REFERENCE_DIR ${SAMTOOLS_SIF} \
-    samtools sort \
-    --threads 20 \
-    --reference ${REFERENCE} \
-    -o ${OUTPUT_BASENAME}_mm_rg_sort.cram \
+    -o ${OUTPUT_BASENAME}_pbmmLike_rg_sort.cram \
     --output-fmt CRAM \
     --write-index \
     -
